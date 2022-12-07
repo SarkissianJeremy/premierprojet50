e191a5935b569826d3ab405237e764b5b9852bec
j974g <j974g@DESKTOP-0GNRN53>
2022-12-07 11:50:44 +0100

    Tp 2 cv

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Employe;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Lieu;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class EmployeController extends AbstractController
 {
@@ -17,4 +18,31 @@ class EmployeController extends AbstractController
         $titre = "employés numéro " . $id;
         return $this->render('principal/voir.html.twig', compact('titre', 'employe'));
     }
+    
+    #[Route('/employe/voirnomb/{nom}', name: 'voirnomb', requirements:["nom"=>"[B][a-zàéèêçîô-]*"], options:["utf8"=> true])]
+    Public function voirnomb(string $nom): Response {
+                return $this->render('principal/voirnomb.html.twig', array(
+            "nom" => $nom
+        ));
+    }
+    
+    public function redirection(string $nom): RedirectionResponse{
+        return $this->redirectToRoute("employe_voirnomb", ['nom'=>"Bond"]);
+    }
+    
+    
+    #[Route('/employe/{_locale}/cv', name: 'cv', requirements:["_locale" => "fr|en"], defaults:["_locale"=>"fr"])]
+    public function cv(string $langue):response {
+        if ($langue == "fr"){
+            return $this->render("cvfr.pdf");
+        }else{
+            return $this->render("principal/cven.pdf");
+        }
+
+
+    }
+    
+        
+        
+    
 }
